e82f4436a31603b30b82fe43c6b810bcf0a417d1
Angelika Brown <angelikpbrown@gmail.com>
2025-03-22 18:53:22 -0400

    read_json

--- jason_helper.py
+++ jason_helper.py
@@ -1,15 +1,17 @@
 import json
 
-def read_json(x):
-
-    y = json.loads(x)
-    return y
+def read_json():
+    f = open('mario.json')
+    data = json.load(f)
+    return data
 
 def read_all_json_files():
     pass
 
 def write_pickle():
+    # pickle.dumps()
     pass
 
 def load_pickle():
+    # pickle.loads()
     pass
