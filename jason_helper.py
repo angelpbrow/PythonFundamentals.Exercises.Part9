f8386f984636603663035a0d4137b8fe363579e8
Angelika Brown <angelikpbrown@gmail.com>
2025-03-23 18:00:47 -0400

    Finished. Still needs testing

--- jason_helper.py
+++ jason_helper.py
@@ -1,5 +1,7 @@
 import json
 import os
+import pickle
+
 
 #function to turn json file into python object -- json.load()
 def read_json(file_path):
@@ -20,12 +22,17 @@ def read_all_json_files(file_path):
     return result
 
 def write_pickle(file_path, data):
-    # pickle.dumps()
-    pass
+    obj = pickle.dumps(data)
+    with open('super_smash_characters.pickle', 'w') as w:
+        w.write(obj)
 
-def load_pickle():
-    # pickle.loads()
-    pass
+
+def load_pickle(file_path):
+    with open(file_path, 'r') as f:
+        obj = (pickle.load(f))
+        print(obj)
+
+
 
 
 read_json('/Users/angel/Desktop/Projects/Exercise9_2025/PythonFundamentals.Exercises.Part9/data/super_smash_bros/link.json')
