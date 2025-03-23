8e815a4fd9989b2e37a793eb68590e31f670e10d
Angelika Brown <angelikpbrown@gmail.com>
2025-03-22 20:25:17 -0400

    It's pickle time

--- jason_helper.py
+++ jason_helper.py
@@ -1,17 +1,31 @@
 import json
+import os
 
+#function to turn json file into python object -- json.load()
 def read_json(file_path):
     with open(file_path, 'r') as f:
         data = json.load(f)
+    print (data)
     return data
 
-def read_all_json_files():
-    pass
+def read_all_json_files(file_path):
+    #given a file_path. for the files in file_path, create a new path
+    #read the new file path and place in a variable
+    #append the results in a list
+    result = []
+    for file in os.listdir(file_path):
+        new_file_path = "%s/%s" % (file_path, file)
+        data = read_json(new_file_path)
+        result.append(data)
+    return result
 
-def write_pickle():
+def write_pickle(file_path, data):
     # pickle.dumps()
     pass
 
 def load_pickle():
     # pickle.loads()
     pass
+
+
+read_json('/Users/angel/Desktop/Projects/Exercise9_2025/PythonFundamentals.Exercises.Part9/data/super_smash_bros/link.json')
